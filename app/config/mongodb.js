2e041c274305b10d7d138337edc7000596c4329e
coopcafe <mr1773393@gmail.com>
2024-10-21 15:47:03 -0500

    Resolviendo conflictos

--- app/config/mongodb.js
+++ app/config/mongodb.js
@@ -1,7 +1,9 @@
 // config/mongodb.js
 import { MongoClient, ServerApiVersion } from 'mongodb';  // Usa import en lugar de require
 
-const uri = "mongodb+srv://mr1773393:Pancho12@clusterempresacafe.vwx5p.mongodb.net/?retryWrites=true&w=majority&appName=Clusterempresacafe";
+const uri = "mongodb+srv://mr1773393:Pancho12@clusterempresacafe.vwx5p.mongodb.net/nombredetubasededatos?retryWrites=true&w=majority&tls=true&appName=Clusterempresacafe";
+
+
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -9,7 +11,13 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
+  tls: true,  // Asegura el uso de TLS
+  tlsAllowInvalidCertificates: true, // Permite certificados no válidos (usar solo en desarrollo)
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  connectTimeoutMS: 10000, // Tiempo de espera para conectar (en milisegundos)
+  socketTimeoutMS: 45000,  // Tiempo de espera del socket (en milisegundos)
 });
 
 async function connect() {
